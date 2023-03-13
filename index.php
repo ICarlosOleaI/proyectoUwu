53050757da0b3f6137495f5965e392403c983498
ICarlosOleaI <87344954+ICarlosOleaI@users.noreply.github.com>
2023-03-13 02:56:00 -0600

    vefdvsdf

--- index.php
+++ index.php
@@ -1,24 +1,63 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Mi página de inicio</title>
+	<title>Página de inicio</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			background-color: #f2f2f2;
+			font-family: Arial, sans-serif;
+		}
+
+		.header {
+			background-color: #333;
+			color: #fff;
+			padding: 20px;
+			text-align: center;
+		}
+
+		ul {
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+			overflow: hidden;
+			background-color: #333;
+		}
+
+		li {
+			float: left;
+		}
+
+		li a {
+			display: block;
+			color: white;
+			text-align: center;
+			padding: 14px 16px;
+			text-decoration: none;
+		}
+
+		li a:hover {
+			background-color: #111;
+		}
+	</style>
 </head>
 <body>
-	<header>
-		<h1>Bienvenido a mi sitio web</h1>
-	</header>
-	<nav>
-		<ul>
-			<li><a href="#">Inicio</a></li>
-			<li><a href="#">Acerca de</a></li>
-			<li><a href="#">Contacto</a></li>
-		</ul>
-	</nav>
-	<main>
-		<p>¡Hola! Bienvenido a mi página de inicio.</p>
-	</main>
-	<footer>
-		<p>&copy; <?php echo date("Y"); ?> Mi sitio web</p>
-	</footer>
+	<div class="header">
+		<h1>Menú de navegación</h1>
+	</div>
+
+	<ul>
+		<li><a class="active" href="#">Inicio</a></li>
+		<li><a href="#">Página 1</a></li>
+		<li><a href="#">Página 2</a></li>
+		<li><a href="#">Página 3</a></li>
+	</ul>
+
+	<div style="padding:20px;margin-top:30px;background-color:#fff;">
+		<h2>Contenido de la página de inicio</h2>
+		<p>Este es el contenido de la página de inicio.</p>
+	</div>
+
 </body>
 </html>
